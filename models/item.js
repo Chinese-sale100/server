f38e69c564c5118486e8c2029b19a551c01daf00
tamar_frid <tf2002565@gmail.com>
2022-03-27 14:10:09 +0300

    add item controller

--- models/item.js
+++ models/item.js
@@ -4,9 +4,7 @@ const itemSchema=new mongoose.Schema({
     item_name:String,
     item_description:String,
     item_price:Number
-
 });
-
 const Item=mongoose.model("items",itemSchema);
 module.exports={
     Item,
