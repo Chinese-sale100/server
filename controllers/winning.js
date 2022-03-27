46b676e19043043b60f32d1ac8c2b7c3b9d531fd
Rachely-Braverman <rachelb55677@gmail.com>
2022-03-27 13:54:54 +0300

    addNewWinningController

--- controllers/winning.js
+++ controllers/winning.js
@@ -1,42 +1,41 @@
-const Winning=require("../models/Winning").Winning;
-const User=require("../models/User").user;
+const Winning = require("../models/Winning").Winning;
+const User = require("../models/User").user;
 
-const addWinning=async(req,res)=>{
+const addWinning = async (req, res) => {
     try {
-        let w=new Winning(req.body);
+        let w = new Winning(req.body);
         await w.save;
         return res.send(w)
     } catch (error) {
         return res.status(400).send(error);
     }
 }
-
-const getWinnerByWinningId=async(req, res)=>{
+const getWinnerByWinningId = async (req, res) => {
     try {
-        let w=await Winning.findById(req.params.id);
+        let w = await Winning.findById(req.params.id);
         return res.send(w);
     } catch (error) {
         return res.status(400).send(error);
     }
 }
-const getWinnerByProductId=async(req, res)=>{
-    let pid=req.params.id
+const getWinnerByProductId = async (req, res) => {
+    let pid = req.params.id
     try {
-        let w=await Winning.find({item_id:pid});
+        let w = await Winning.find({ item_id: pid });
         return res.send(w);
     } catch (error) {
         return res.status(400).send(error);
     }
 }
-const getUsersByProductId=async(req, res)=>{
-    let pid=req.params.id;
+const getUsersByProductId = async (req, res) => {
+    let pid = req.params.id;
     try {
-        let users = await User.find({}).filter(user=>{user.arr_orders.find(order=>order.item_id==pid)!=-1});
+        let users = await User.find({}).filter(user => { user.arr_orders.find(order => order.item_id == pid) != -1 });
         return res.send(users);
     } catch (error) {
         return res.status(400).send(error);
     }
 }
 module.exports = {
-    addWinning,getWinnerByWinningId, getWinnerByProductId, getUsersByProductId
+    addWinning, getWinnerByWinningId, getWinnerByProductId, getUsersByProductId
 }
