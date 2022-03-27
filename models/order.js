b1cfcf88157c56ff4313b55af625d689a8eb374a
YehuditRozovsky <r036166146@gmail.com>
2022-03-27 13:45:21 +0300

    add order schema

---
+++ models/order.js
@@ -0,0 +1,11 @@
+
+const mongoose=require("mongoose")
+
+const orderSchema=new mongoose.Schema({
+    item_id:Number,
+    item_name:String,
+    item_description:String,
+    price:Number
+})
+
+module.export=orderSchema
