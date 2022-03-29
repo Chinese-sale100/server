52050a61fe19ef74e1c07aea58e015e1942dc9ea
YehuditRozovsky <r036166146@gmail.com>
2022-03-29 20:40:46 +0300

    basicCode

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const express = require("express");
 const winningRoute = require("./routes/winning");
 const userRoute = require("./routes/user");
-
 const app = express();
 mongoose.connect("mongodb://localhost:27017/chinese-sale100")
     .then(() => { console.log("mongo db connected"); })
