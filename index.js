52db76d7f4671ab5d332b2d7342d914662bc07fd
YehuditRozovsky <r036166146@gmail.com>
2022-03-29 20:36:34 +0300

    basic code

---
+++ index.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const winningRoute = require("./routes/winning");
+const userRoute = require("./routes/user");
+
+const app = express();
+mongoose.connect("mongodb://localhost:27017/chinese-sale100")
+    .then(() => { console.log("mongo db connected"); })
+    .catch(er => { console.log(er) });
+
+app.use(express.json())
+app.use("/winning", winningRoute)
+app.use("/user", userRoute)
+app.listen(4500, () => { console.log("waiting") })
