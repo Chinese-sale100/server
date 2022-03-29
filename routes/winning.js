0b792efb4167625fbef47cc58bd94574de706257
YehuditRozovsky <r036166146@gmail.com>
2022-03-29 21:06:51 +0300

    add id to url

--- routes/winning.js
+++ routes/winning.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const winningController=require("../controllers/winning");
 
-router.get("",winningController.getWinnerByProductId);
-router.get("/:id",winningController.getWinnerByWinningId);
+router.get("/:productId",winningController.getWinnerByProductId);
+router.get("/:winningId",winningController.getWinnerByWinningId);
 router.post("",winningController.addWinning);
 
 module.exports=router;
